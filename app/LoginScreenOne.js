7b541d23cebf4a17fac4f60816758242c2dfa6b6
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2023-04-19 14:41:54 +0530

    text inputs added

--- app/LoginScreenOne.js
+++ app/LoginScreenOne.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 
 function LoginScreenOne() {
+
+    const [text, onChangeText] = React.useState('Username or Email');
+    const[pwtext, pwChangeTex] =React.useState('Password');
   return (
   
        <View style={styles.container}>
         <View style={styles.view1}>
                 <Text style={styles.heading1}>Hello Again!</Text>
-                <Text>Welcome back you've been missed!</Text>
+                <Text style={styles.para1}>Welcome back you've been missed!</Text>
         </View>
 
         <View style={styles.view2}>
+            <TextInput style={styles.email} value={text} onChangeText={onChangeText}/>
+            <TextInput style={styles.email} value={pwtext} onChangeText={pwChangeTex}/>
+            <Text style={styles.fogPw}>Forgot Password</Text>
                 
         </View>
        </View>
@@ -31,21 +37,37 @@ const styles = StyleSheet.create({
     view1:{
         flex:1,
         backgroundColor:'white',
+        top:'8%'
 
     },
 
     view2:{
         flex:2,
-        backgroundColor:'green',
+        
     },
-
-
 
     //text styles
     heading1:{
         fontSize:30,
         fontWeight:'bold',
+        textAlign:'center'
+
+    },
+
+    para1:{
+        fontSize:18,
+        opacity:0.7,
+        textAlign:'center',
+        paddingHorizontal:'20%',
+        top:3
         
+    },
+
+    email:{
+        height: 50,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
     }
     
 })
